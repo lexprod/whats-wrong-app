383ae1628a9eeef50b6b135aeaa94cb5190c204c
lexprod <lexprod@gmail.com>
2024-04-28 11:44:45 -0700

    preshow update

--- src/app/shared/QUESTIONS.js
+++ src/app/shared/QUESTIONS.js
@@ -2,92 +2,248 @@ export const QUESTIONS = [
   {
     id: 1,
     qtext:
-      "Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam? Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam??",
-    answersNum: 6,
-    correctIndex: 4,
+      "In Taiwan, you’re allowed to change your name up to three times, but in March 2021 a government administrator asked people to stop changing their names to what?",
+    answersNum: 3,
+    correctIndex: 1,
     answersArray: [
       {
         id: 0,
         letter: "A",
-        text: "Morbi quis commodo of the show.",
+        text: "Emojis",
       },
       {
         id: 1,
         letter: "B",
-        text: "Being a Morbi quis commodo.",
+        text: "Salmon",
       },
       {
         id: 2,
         letter: "C",
-        text: "Wearing hiMorbi quis commodoaresd.",
-      },
-      {
-        id: 3,
-        letter: "D",
-        text: "Morbi quis commodo toes.",
-      },
-      {
-        id: 4,
-        letter: "E",
-        text: "Being Morbi quis commodo.",
-      },
-      {
-        id: 5,
-        letter: "F",
-        text: "Morbi quis commodonight.",
+        text: "Sunflower",
       },
     ],
   },
   {
     id: 2,
     qtext:
-      "Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam?",
-    answersNum: 4,
-    correctIndex: 2,
-    answersArray: [
-      {
-        id: 0,
-        letter: "A",
-        text: "Morbi quis commodo",
-      },
-      {
-        id: 1,
-        letter: "B",
-        text: "eiusmod tempor",
-      },
-      {
-        id: 2,
-        letter: "C",
-        text: "amet tellus cras",
-      },
-      {
-        id: 3,
-        letter: "D",
-        text: "Having no toes.",
-      },
-    ],
-  },
-  {
-    id: 3,
-    qtext:
-      "But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system?",
+      "Lelush, a contestant on Tencent’s making the boy band show 'Produce Camp 2021', made it all the way to the finals despite what?",
     answersNum: 3,
     correctIndex: 2,
     answersArray: [
       {
         id: 0,
         letter: "A",
-        text: "To take a trivial example.",
+        text: "Not knowing any Chinese",
       },
       {
         id: 1,
         letter: "B",
-        text: "righteous indignation and dislike men.",
+        text: "Being an androgynous Woman",
       },
       {
         id: 2,
         letter: "C",
-        text: "equal blame.",
+        text: "Asking viewers to not vote for them",
+      },
+    ],
+  },
+  {
+    id: 3,
+    qtext:
+      "In 2013, a group called Planetary Resources ran a $1.5 Million Dollar Kickstarter campaign. Sadly in 2016, they announced they were refunding all pledges. What was the never-fulfilled $25 pledge level reward?",
+    answersNum: 3,
+    correctIndex: 1,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "An Authentic Meteorite",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "A Space Selfie",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "A Time Gem",
+      },
+    ],
+  },
+  {
+    id: 4,
+    qtext: "The term “sybarite” describes what kind of person?",
+    answersNum: 3,
+    correctIndex: 0,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "An Opulent Pleasure Seeker",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "An Untrained Orchestra Conductor",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "A Slobbering Dirty Person",
+      },
+    ],
+  },
+  {
+    id: 5,
+    qtext: "Which of these animals does NOT fart?",
+    answersNum: 4,
+    correctIndex: 0,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "Penguins",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "Termites",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "Manatees",
+      },
+      {
+        id: 3,
+        letter: "D",
+        text: "Iguanas",
+      },
+    ],
+  },
+  {
+    id: 6,
+    qtext:
+      "The Byerley Turk, the Darley Arabian, and the Godolphin Barb were all athletes in what 17th century sport?",
+    answersNum: 4,
+    correctIndex: 0,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "Thoroughbred Horse Racing",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "Hot Air Ballooning",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "Darts",
+      },
+      {
+        id: 3,
+        letter: "D",
+        text: "Ten Pin Bowling",
+      },
+    ],
+  },
+  {
+    id: 7,
+    qtext: "Which of these musical groups performed at Woodstock?",
+    answersNum: 4,
+    correctIndex: 2,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "The Electric Prunes",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "Nazz",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "Country Joe and the Fish",
+      },
+      {
+        id: 3,
+        letter: "D",
+        text: "The Machine Elf",
+      },
+    ],
+  },
+  {
+    id: 8,
+    qtext:
+      "Originally shown in 1897, what was the subject of the first ever Venezuelan film?",
+    answersNum: 4,
+    correctIndex: 0,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "Pulling Teeth",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "Young People Swimming",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "Feeding Sea Lions",
+      },
+      {
+        id: 3,
+        letter: "D",
+        text: "A Naval Blockade",
+      },
+    ],
+  },
+  {
+    id: 9,
+    qtext:
+      "When Theodore Roosevelt went on an African Expedition to collect animal and plant specimens for the Smithsonian, he took along his Pigskin what?",
+    answersNum: 6,
+    correctIndex: 4,
+    answersArray: [
+      {
+        id: 0,
+        letter: "A",
+        text: "Pigskin Kettle",
+      },
+      {
+        id: 1,
+        letter: "B",
+        text: "Pigskin Blackjack",
+      },
+      {
+        id: 2,
+        letter: "C",
+        text: "Pigskin Underwear",
+      },
+      {
+        id: 3,
+        letter: "D",
+        text: "Pigskin Chew Toy",
+      },
+      {
+        id: 4,
+        letter: "E",
+        text: "Pigskin Library",
+      },
+      {
+        id: 5,
+        letter: "F",
+        text: "Pigskin Umbrella Hat",
       },
     ],
   },
--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Container, Col } from 'reactstrap';
+import { Container, Col } from "reactstrap";
 
 const QuestionBox = ({ question }) => {
-    return (
-        <Col className="d-flex align-items-center justify-content-center rounded-5" style={{
-            height: '40vh', backgroundColor: '#A06B9A', color: 'black', textAlign: 'center'
-        }}>
-            <h2>
-                {question.qtext}
-            </h2>
-        </Col>
-    );
-}
+  return (
+    <Col
+      className="d-flex align-items-center justify-content-center rounded-5"
+      style={{
+        height: "40vh",
+        backgroundColor: "#A06B9A",
+        color: "black",
+        textAlign: "center",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+      }}
+    >
+      <h2>{question.qtext}</h2>
+    </Col>
+  );
+};
 
 export default QuestionBox;
